5968e01bd085d73573a697841935a7c2b0c3b3ec
Nishchitha <nishushetty151997@gmail.com>
2020-02-14 14:56:24 +0530

    fc

---
+++ TestProj.Tests/Controllers/EmployeeControllerTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Web.Mvc;
+
+namespace TestProj.Tests.Controllers
+{
+    class EmployeeControllerTest
+    {
+        public void Employees()
+        {
+            // Arrange
+            EmployeeControllerTest controller = new EmployeeControllerTest();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        private ViewResult Index()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
